38c28964d49576f4a015008fbd0f471cb902fd57
HafSimba <gianlugiaffreda@gmail.com>
2026-04-12 15:50:30 +0200

    inizializzato progetto completo ''Artigiani''

---
+++ EsempiCompleti/ArtigianiLocali/backendartigiani/src/main/java/backendartigiani/BackendartigianiApplication.java
@@ -0,0 +1,13 @@
+package backendartigiani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendartigianiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendartigianiApplication.class, args);
+	}
+
+}
---
+++ EsempiCompleti/ArtigianiLocali/backendartigiani/src/test/java/backendartigiani/BackendartigianiApplicationTests.java
@@ -0,0 +1,13 @@
+package backendartigiani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendartigianiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
